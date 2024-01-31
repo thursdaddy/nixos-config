1156d09ea11d1bfd5b29a981d888ae2bde3fd68f
thursdaddy <thursdaddy@pm.me>
2024-01-31 12:50:57 -0700

    reset: archive builds/ dir

---
+++ .archive/builds/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Build image";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  outputs = { self, nixpkgs }: rec {
+    nixosConfigurations.rpi2 = nixpkgs.lib.nixosSystem {
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-raspberrypi.nix"
+        {
+          nixpkgs.config.allowUnsupportedSystem = true;
+        }
+      ];
+    };
+    images.rpi2 = nixosConfigurations.rpi2.config.system.build.sdImage;
+    nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ({ pkgs, ... }: {
+            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+            environment.systemPackages = [ pkgs.neovim pkgs.tmux pkgs.git ];
+            systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+            users.users.root.openssh.authorizedKeys.keys = [
+              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDf3iS7lXUebJun3jQ3EJWkFoZcCrfaaAJaZWE1FFqEkLUFXhuBxITRXXqVyPjBHrY52RoAmg6RQejQDBcyV4sSs1IWMhHr50RzdM1FXGJunel6l3gvg36vUZ8OU+KU3N7E41it8we8IvV+QeDfV3QhXWAvCHIwA7UdTha00YDPaZxmZPOOnq0tE16d/9u8F8jYyuuBPwtE8PilaY5q6HI151hNTxb7vxru3H6faUjO1JKnY3UjU32FyTkz4o4IkvmdAWoft38gmtdr1VU0Fg/aZ8H6ltUPGdj8d/Nr6iUvxT41cIMmPNEeKJQ1mrVlIZ2AMN9LggLVIx02LbIQ2Pabbvyhq4FHCTztYkYjPnBBEbqKcsSMObqzGhQQxiOkrbVjmx8qei0NvnmPUHpoPCKzcJhApTBRKd7Pck2+nl56BJG9YqnELjAiogolELyJgnB88g4zKKGi/o21GW1vRXGMMn/gCWkiPBjBlBzYjGDaVFfMLc9GVhVfgnJFFiVZmMk= thurs@nixos"
+            ];
+          })
+      ];
+    };
+    images.iso = nixosConfigurations.iso.config.system.build.isoImage;
+  };
+}
+
+
