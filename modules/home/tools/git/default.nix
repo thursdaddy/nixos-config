6158f9d84d30e981a6fde4375e1ec6ba1c300eb5
thurs <thursdaddy@pm.me>
2024-03-04 15:36:04 -0700

    feat: udpate zsh aliases for git

--- modules/home/tools/git/default.nix
+++ modules/home/tools/git/default.nix
@@ -27,9 +27,29 @@ in {
 
       programs.zsh.shellAliases = {
         "gaa" = "git add .";
-        "gst" = "git -P status";
-        "gd" = "git -P diff";
-        "gds" = "git -P diff --staged";
+        "gc" = "git commit";
+        "gca" = "git commit --amend";
+        "gcg" = "git --no-pager log --graph --topo-order --abbrev-commit --date=short --decorate --all --boundary";
+        "gcl" = "git --no-pager log --topo-order --abbrev-commit --date=short --decorate --all --boundary --reverse";
+        "gco" = "git checkout";
+        "gcob" = "git checkout -b";
+        "gcom" = "git checkout main";
+        "gcm" = "git commit --message";
+        "gd" = "git --no-pager diff";
+        "gD" = "git diff";
+        "gds" = "git --no-pager diff --staged";
+        "gf" = "git fetch";
+        "gfo" = "git fetch --origin";
+        "gfp" = "git push --set-upstream origin `git symbolic-ref --short HEAD`";
+        "gl" = "git pull";
+        "gp" = "git push";
+        "grsh" = "git reset --soft HEAD^";
+        "grh" = "git reset";
+        "grhh" = "git reset --hard";
+        "gru" = "git reset --";
+        "grset" = "git remote set-url";
+        "gsa" = "git stash --all";
+        "gst" = "git --no-pager status";
       };
     };
   };
