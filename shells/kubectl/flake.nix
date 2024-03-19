2bd9797e6fc3d61f54c8d63b857e57bde6388841
thurs <thursdaddy@pm.me>
2024-03-19 15:18:33 -0700

    feat: add pulumi, kubectl shells

--- shells/kubectl/flake.nix
+++ shells/kubectl/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Terraform development environment";
+  description = "A Nix-flake-based Kubectl development environment";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
@@ -7,15 +7,17 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        pkgs = import nixpkgs { inherit system; };
       });
     in
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            terraform
+            kubectl
+            # AWS cli
             awscli2
+
           ];
         };
       });
--- shells/pulumi/flake.nix
+++ shells/pulumi/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Terraform development environment";
+  description = "A Nix-flake-based Pulumi development environment";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
@@ -7,15 +7,21 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        pkgs = import nixpkgs { inherit system; };
       });
     in
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            terraform
+            pulumi-bin
+
+            # Python SDK
+            python311
+
+            # AWS cli
             awscli2
+
           ];
         };
       });
