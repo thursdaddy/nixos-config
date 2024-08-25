35b1aa7ecd1c5aa746fd76529d6eb4a5fcd3b926
thurs <thursdaddy@pm.me>
2024-08-25 11:46:21 -0700

    wip: travelpi

---
+++ hosts/travelpi/configuration.nix
@@ -0,0 +1,80 @@
+{ pkgs, lib, config, inputs, hostname, ... }:
+with lib;
+with lib.thurs;
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.raspberry-pi-4
+    ./hardware-configuration.nix
+    ../../overlays/unstable
+    ../../modules/nixos/import.nix
+    ../../modules/home/import.nix
+  ];
+
+  config = {
+    system.stateVersion = "24.05";
+
+    environment.systemPackages = with pkgs; [
+      neovim
+    ];
+
+    mine = {
+      user = {
+        enable = true;
+        home-manager = enabled;
+        ssh-config = enabled;
+      };
+
+      cli-tools = {
+        bottom = enabled;
+        sops = {
+          enable = true;
+          defaultSopsFile = (inputs.nixos-thurs.packages.${pkgs.system}.mySecrets + "/encrypted/netpi.yaml");
+        };
+        tmux = enabled;
+      };
+
+      services = {
+        tailscale = {
+          enable = true;
+          authKeyFile = config.sops.secrets."tailscale/AUTH_KEY".path;
+          useRoutingFeatures = "client";
+          extraUpFlags = [ "--advertise-routes=192.168.20.0/24" ];
+        };
+        prometheus = {
+          enable = true;
+          exporters.node = enabled;
+        };
+      };
+
+      system = {
+        desktop = {
+          kde = enabled;
+        };
+        networking = {
+          networkmanager = {
+            enable = true;
+            hostname = "travelpi";
+          };
+          firewall = enabled;
+          forwarding.ipv4 = true;
+        };
+        nix = {
+          unfree = enabled;
+          flakes = enabled;
+        };
+        security.sudonopass = enabled;
+        services = {
+          openssh = enabled;
+        };
+        shell.zsh = enabled;
+        utils = enabled;
+        virtualisation = {
+          docker = {
+            enable = true;
+            scripts.check-versions = true;
+          };
+        };
+      };
+    };
+  };
+}
---
+++ hosts/travelpi/hardware-configuration.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, ... }: {
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.raspberry-pi."4".poe-plus-hat.enable = true;
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+}
