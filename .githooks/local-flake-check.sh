a09f253fb71367a18d4a7891504f942185730557
thurs <thursdaddy@pm.me>
2024-12-05 11:19:35 -0700

    fix: local-flake-check fix

--- .githooks/local-flake-check.sh
+++ .githooks/local-flake-check.sh
@@ -1,8 +1,11 @@
 #!/run/current-system/sw/bin/bash
+set -euo pipefail
 
-if git --no-pager diff --staged --name-only | grep -q flake.nix | grep -q "file://" flake.nix; then
-  printf "Flake.nix contains local path for an input, please fix before committing to repo"
-  exit 1
+if git --no-pager diff --staged --name-only | grep -q flake.nix; then
+  if grep -q "file://" flake.nix; then
+    printf "Flake.nix contains local path for an input, please fix before committing to repo"
+    exit 1
+  fi
 else
   exit 0
 fi
