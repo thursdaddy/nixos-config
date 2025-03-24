5efc796e47711a8fad0217492102c962465e0164
thurs <thursdaddy@pm.me>
2025-03-24 13:38:34 -0700

    feat: c137 sleep-on-lan via hass
    - fix hyprlock not triggering before sleep
    - add boolean to hass used by appdaemon

---
+++ modules/nixos/desktop/addons/hyrplock/default.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+
+  inherit (lib) mkIf;
+  inherit (config.mine) user;
+  cfg = config.mine.desktop.hyprlock;
+
+in
+{
+  config = mkIf cfg.enable {
+    systemd.services.lock-on-suspend = {
+      description = "Lock on suspend";
+      wantedBy = [
+        "sleep.target"
+        "suspend.target"
+        "hibernate.target"
+        "hybrid-sleep.target"
+      ];
+      before = [
+        "sleep.target"
+        "suspend.target"
+        "hibernate.target"
+        "hybrid-sleep.target"
+      ];
+      environment = {
+        DISPLAY = ":0";
+        WAYLAND_DISPLAY = "wayland-1";
+        XDG_RUNTIME_DIR = "/run/user/1000";
+      };
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${lib.getExe pkgs.hyprlock}";
+        User = user.name;
+      };
+    };
+
+    # hyprlock needs a secong to complete before suspending
+    systemd.services.sleep-before-suspend = {
+      description = "Sleep before suspend";
+      wantedBy = [
+        "sleep.target"
+        "suspend.target"
+        "hibernate.target"
+        "hybrid-sleep.target"
+      ];
+      before = [
+        "sleep.target"
+        "suspend.target"
+        "hibernate.target"
+        "hybrid-sleep.target"
+      ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStartPre = "/run/current-system/sw/bin/sleep 1";
+        ExecStart = "/run/current-system/sw/bin/true";
+      };
+    };
+  };
+}
