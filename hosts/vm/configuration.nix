be075fb599c87a2590360d8837d70da787314958
thurs <thursdaddy@pm.me>
2025-03-26 15:11:52 -0700

    wip: qcow2

---
+++ hosts/vm/configuration.nix
@@ -0,0 +1,73 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  inherit (config.mine) user;
+  inherit (lib.thurs) enabled;
+in
+{
+
+  imports = [
+    ../../modules/nixos/import.nix
+    ../../modules/shared/import.nix
+    ../../modules/home/import.nix
+  ];
+
+  config = {
+    system.stateVersion = "24.11";
+
+    # mine.user.enable does not set initialPassword since it uses SSH Keys by default
+    users.users.thurs.initialPassword = "changeme";
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+    mine = {
+      user = {
+        enable = true;
+        home-manager = enabled;
+        shell.package = pkgs.fish;
+      };
+
+      home-manager = {
+        git = enabled;
+        tmux = {
+          enable = true;
+          sessionizer = {
+            enable = true;
+            searchPaths = [
+              "${user.homeDir}/projects/nix"
+              "${user.homeDir}/projects/cloud"
+              "${user.homeDir}/projects/homelab"
+              "${user.homeDir}/projects/personal"
+            ];
+          };
+        };
+        zsh = enabled;
+      };
+
+      cli-tools = {
+        direnv = enabled;
+        git = enabled;
+      };
+
+      services = {
+        docker = enabled;
+      };
+
+      system = {
+        networking = {
+          networkd = {
+            enable = true;
+            hostname = "nixos";
+          };
+          forwarding.ipv4 = true;
+          resolved = enabled;
+        };
+        security.sudonopass = enabled;
+        utils = enabled;
+      };
+    };
+  };
+}
