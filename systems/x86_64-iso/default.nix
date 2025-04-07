ad2a43d3772f73ca60b5752080aa5c0568da3aba
thursdaddy <thursdaddy@pm.me>
2025-04-07 16:11:26 -0700

    feat: iso for proxmox

--- systems/x86_64-iso/default.nix
+++ systems/x86_64-iso/default.nix
@@ -13,25 +13,22 @@
     };
 
     hardware.cpu.amd.updateMicrocode = true;
-    boot.supportedFilesystems = [ "zfs" ];
-    boot.zfs.forceImportRoot = false;
+
+
+    systemd.network.enable = true;
 
     networking = {
-      hostId = "00000000";
-      defaultGateway = "192.168.20.1";
-      nameservers = [
-        "192.168.10.57"
-        "192.168.10.201"
-      ];
-      interfaces.eth0.ipv4.addresses = [
-        {
-          address = "192.168.20.222";
-          prefixLength = 24;
-        }
-      ];
+      useDHCP = true;
+      useNetworkd = true;
+      hostName = "nixos";
     };
 
-    services.openssh.enable = true;
+    services = {
+      qemuGuest.enable = true;
+      openssh.enable = true;
+    };
+
+    users.users.root.initialPassword = "changeme";
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsmsLubwu6s0wkeKTsM2EIuJRKFsg2nZdRCVtQHk9LT thurs"
     ];
