248bf5593994a004b0185777f594612e52f5c2b9
thurs <thursdaddy@pm.me>
2026-03-24 14:44:11 -0700

    fix: touchsudo in tmux

--- modules/base/touchsudo.nix
+++ modules/base/touchsudo.nix
@@ -4,6 +4,15 @@ _: {
     {
       security.pam.services.sudo_local.touchIdAuth = true;
 
+      # fix touchid auth in tmux
+      environment = {
+        etc."pam.d/sudo_local".text = ''
+          # Managed by Nix Darwin
+          auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
+          auth       sufficient     pam_tid.so
+        '';
+      };
+
       environment.systemPackages = [
         pkgs.pam-reattach
       ];
