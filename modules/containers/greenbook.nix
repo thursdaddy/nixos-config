8096476550de182152ecf4599d72f7a0548cfbf5
thurs <thursdaddy@pm.me>
2026-04-27 14:49:51 -0700

    fix: greenbook-db traefik tcp

--- modules/containers/greenbook.nix
+++ modules/containers/greenbook.nix
@@ -8,7 +8,7 @@ _: {
     }:
     let
       name = "greenbook";
-      version = "0.0.7";
+      version = "0.1.0";
 
       cfg = config.mine.containers.${name};
       fqdn = "${cfg.subdomain}.${config.mine.containers.traefik.rootDomainName}";
@@ -39,77 +39,87 @@ _: {
       };
 
       config = lib.mkIf cfg.enable {
-        virtualisation.oci-containers.containers."${dbName}" = {
-          image = "postgres:17.6-alpine";
-          hostname = dbName;
-          ports = [
-            "5432"
-          ];
-          volumes = [
-            "${config.mine.containers.settings.configPath}/greenbook/db:/var/lib/postgresql/data"
-            "${config.mine.containers.settings.configPath}/greenbook/db_dumps:/db_dumps"
-          ];
-          extraOptions = [
-            "--network=traefik"
-            "--pull=always"
-          ];
-          environmentFiles = [
-            config.sops.templates."greenbook-db".path
-          ];
-          environment = {
-            POSTGRES_USER = "receipts";
-            POSTGRES_DB = "receipts";
-            PGDATA = "/var/lib/postgresql/data/pgdata";
+        virtualisation.oci-containers.containers = {
+          "${name}" = {
+            image = "gitea.thurs.pw/homelab/greenbook:v${version}";
+            login = {
+              username = "thurs";
+              registry = "gitea.thurs.pw";
+              passwordFile = config.sops.templates."registry_pass".path;
+            };
+            hostname = name;
+            ports = [
+              "8000"
+            ];
+            volumes = [
+              "${config.mine.containers.settings.configPath}/greenbook/app:/app/data"
+            ];
+            extraOptions = [
+              "--network=traefik"
+              "--pull=always"
+            ];
+            environmentFiles = [
+              config.sops.templates."greenbook-app".path
+            ];
+            environment = {
+              PAPERLESS_API_BASE_URL = "http://paperless-ngx-webserver:8000/api";
+              PAPERLESS_HTTPS_URL = "https://paperless.thurs.pw/api";
+              DB_HOST = "${dbName}";
+              DB_USERNAME = "receipts";
+              DB_NAME = "receipts";
+              DB_PORT = "5432";
+            };
+            labels = {
+              "traefik.enable" = "true";
+              "traefik.http.routers.${name}.tls" = "true";
+              "traefik.http.routers.${name}.tls.certresolver" = "letsencrypt";
+              "traefik.http.routers.${name}.entrypoints" = "websecure";
+              "traefik.http.routers.${name}.rule" = "Host(`${fqdn}`)";
+              "traefik.http.services.${name}.loadbalancer.server.port" = "8000";
+              "enable.versions.check" = "false";
+              "homelab.backup.enable" = "true";
+              "homelab.backup.path" = "${config.mine.containers.settings.configPath}/${name}";
+              "homelab.backup.retention.period" = "5";
+            };
           };
-          labels = {
-            "enable.versions.check" = "false";
-            "homelab.backup.enable" = "true";
-            "homelab.backup.path" = "${config.mine.containers.settings.configPath}/greenbook/db_dumps";
-            "homelab.backup.retention.period" = "5";
-          };
-        };
 
-        virtualisation.oci-containers.containers."${name}" = {
-          image = "gitea.thurs.pw/homelab/greenbook:v${version}";
-          login = {
-            username = "thurs";
-            registry = "gitea.thurs.pw";
-            passwordFile = config.sops.templates."registry_pass".path;
+          "${dbName}" = {
+            image = "postgres:17.6-alpine";
+            hostname = dbName;
+            ports = [
+              "5432"
+            ];
+            volumes = [
+              "${config.mine.containers.settings.configPath}/greenbook/db:/var/lib/postgresql/data"
+              "${config.mine.containers.settings.configPath}/greenbook/db_dumps:/db_dumps"
+            ];
+            extraOptions = [
+              "--network=traefik"
+              "--pull=always"
+            ];
+            environmentFiles = [
+              config.sops.templates."greenbook-db".path
+            ];
+            environment = {
+              POSTGRES_USER = "receipts";
+              POSTGRES_DB = "receipts";
+              PGDATA = "/var/lib/postgresql/data/pgdata";
+            };
+            labels = {
+              "enable.versions.check" = "false";
+              "homelab.backup.enable" = "true";
+              "homelab.backup.path" = "${config.mine.containers.settings.configPath}/greenbook/db_dumps";
+              "homelab.backup.retention.period" = "5";
+              "traefik.enable" = "true";
+              "traefik.tcp.routers.${dbName}.tls" = "true";
+              "traefik.tcp.routers.${dbName}.tls.certresolver" = "letsencrypt";
+              "traefik.tcp.routers.${dbName}.entrypoints" = "websecure";
+              "traefik.tcp.routers.${dbName}.rule" =
+                "HostSNI(`${dbName}.${config.mine.containers.traefik.rootDomainName}`)";
+              "traefik.tcp.services.${dbName}.loadbalancer.server.port" = "5432";
+            };
           };
-          hostname = name;
-          ports = [
-            "8000"
-          ];
-          volumes = [
-            "${config.mine.containers.settings.configPath}/greenbook/app:/app/data"
-          ];
-          extraOptions = [
-            "--network=traefik"
-            "--pull=always"
-          ];
-          environmentFiles = [
-            config.sops.templates."greenbook-app".path
-          ];
-          environment = {
-            PAPERLESS_API_BASE_URL = "http://paperless-ngx-webserver:8000/api";
-            PAPERLESS_HTTPS_URL = "https://paperless.thurs.pw/api";
-            DB_HOST = "${dbName}";
-            DB_USERNAME = "receipts";
-            DB_NAME = "receipts";
-            DB_PORT = "5432";
-          };
-          labels = {
-            "traefik.enable" = "true";
-            "traefik.http.routers.${name}.tls" = "true";
-            "traefik.http.routers.${name}.tls.certresolver" = "letsencrypt";
-            "traefik.http.routers.${name}.entrypoints" = "websecure";
-            "traefik.http.routers.${name}.rule" = "Host(`${fqdn}`)";
-            "traefik.http.services.${name}.loadbalancer.server.port" = "8000";
-            "enable.versions.check" = "false";
-            "homelab.backup.enable" = "true";
-            "homelab.backup.path" = "${config.mine.containers.settings.configPath}/${name}";
-            "homelab.backup.retention.period" = "5";
-          };
+
         };
 
         sops = {
